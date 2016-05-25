5adeb21fa760ea0945367b49c36613801b0e9c0b
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-05-25 07:41:50 -0300

    .

--- lib/ws.go
+++ lib/ws.go
@@ -1,1 +1,23 @@
 package goliv
+
+const (
+	WsContainerId    = "#goliv-container"
+	WsInjectedScript = `
+    <div id="goliv-container" style="display:none">
+      <script>
+        ;(function() {
+          var _protocol = /^https/.test(location.protocol) ? 'wss' : 'ws';
+          var _port = 1234;
+
+          var _ws = new WebSocket(_protocol + "//" + location.host)
+
+          _ws.onmessage = function(ev) {
+          if (ev.data === "reload") {
+            location.reload();
+          }
+        }
+        }());
+      </script>
+    </div>
+  `
+)
--- lib/ws_test.go
+++ lib/ws_test.go
@@ -1,1 +1,36 @@
 package goliv
+
+import "testing"
+
+func TestConstantWsContainerId(t *testing.T) {
+	id := "#goliv-container"
+
+	if WsContainerId != id {
+		t.Errorf("Expected %s to equal %s", WsContainerId, id)
+	}
+}
+
+func TestConstantWsInjectedHtml(t *testing.T) {
+	r := `
+    <div id="goliv-container" style="display:none">
+      <script>
+        ;(function() {
+          var _protocol = /^https/.test(location.protocol) ? 'wss' : 'ws';
+          var _port = 1234;
+
+          var _ws = new WebSocket(_protocol + "//" + location.host)
+
+          _ws.onmessage = function(ev) {
+          if (ev.data === "reload") {
+            location.reload();
+          }
+        }
+        }());
+      </script>
+    </div>
+  `
+
+	if WsInjectedScript != r {
+		t.Errorf("Expected %s to equal %s", WsInjectedScript, r)
+	}
+}
