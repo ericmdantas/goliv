32df8c00f8ece84b37160e151a2b1ec849a3562d
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-05-28 22:55:14 -0300

    ;

--- lib/file.go
+++ lib/file.go
@@ -1,7 +1,24 @@
 package goliv
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+const (
+	mainHtml = "index.html"
+)
 
 func Yo() {
 	fmt.Println("yo!")
+}
+
+func Read() string {
+	str, err := ioutil.ReadFile(mainHtml)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(str)
 }
