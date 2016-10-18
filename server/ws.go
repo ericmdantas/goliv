cfe9df8acab3da27201f2d10d6fa5883963e9597
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-18 07:24:17 -0200

    .

--- server/ws.go
+++ server/ws.go
@@ -4,6 +4,34 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var WSScript = `
+	<div id="goliv-container" style="display: none">
+		;(function() {
+			"use strict";
+			
+			var ws = new WebSocket("ws://127.0.0.1:986/abc");
+			
+			ws.onmessage = function(ev) {
+				if (ev.data === "reload") {
+					window.location.reload();
+				}
+			};
+		}());
+	</div>
+`
+
 type WS struct {
-	conn *websocket.Conn
+	clients []*websocket.Conn
+}
+
+func (ws *WS) OnConnect(conn *websocket.Conn, cb func()) {
+	ws.clients = append(ws.clients, conn)
+
+	cb()
+}
+
+func NewWS() *WS {
+	return &WS{
+		clients: []*websocket.Conn{},
+	}
 }
--- server/ws_test.go
+++ server/ws_test.go
@@ -1,1 +1,34 @@
 package server
+
+import (
+	"testing"
+
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewWS(t *testing.T) {
+	ws := NewWS()
+
+	assert.Equal(t, []*websocket.Conn{}, ws.clients, "should start with an empty client slice")
+}
+
+func TestWSScript(t *testing.T) {
+	script := `
+	<div id="goliv-container" style="display: none">
+		;(function() {
+			"use strict";
+			
+			var ws = new WebSocket("ws://127.0.0.1:986/abc");
+			
+			ws.onmessage = function(ev) {
+				if (ev.data === "reload") {
+					window.location.reload();
+				}
+			};
+		}());
+	</div>
+`
+
+	assert.Contains(t, script, WSScript, "should have the right info for the script")
+}
