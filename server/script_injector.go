089ddd96d68947efd5179900cd7e975e9807f6fa
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-18 16:57:47 -0200

    .

---
+++ server/script_injector.go
@@ -0,0 +1,5 @@
+package server
+
+func InjectScript(o *Options) error {
+	return nil
+}
---
+++ server/script_injector_test.go
@@ -0,0 +1,11 @@
+package server
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInjectScript(t *testing.T) {
+	assert.Equal(t, true, true, "a")
+}
