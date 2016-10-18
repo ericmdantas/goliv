e3dc5dac692f4ba864ddd61ab44e316008a90a37
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-18 16:41:33 -0200

    .

--- server/ws.go
+++ server/ws.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	"github.com/gorilla/websocket"
+	_ "github.com/gorilla/websocket"
 )
 
 var WSScript = `
@@ -19,29 +19,3 @@ var WSScript = `
 		}());
 	</div>
 `
-
-type WSSender interface {
-	WriteMessage(messageType int, payload []byte) error
-}
-
-type WS struct {
-	clients []*WSSender
-}
-
-func (ws *WS) OnConnect(conn WSSender, cb func()) {
-	ws.clients = append(ws.clients, conn)
-
-	cb()
-}
-
-func (ws *WS) BroadcastReload() {
-	for _, client := range ws.clients {
-		client.WriteMessage(websocket.TextMessage, []byte("reload"))
-	}
-}
-
-func NewWS() *WS {
-	return &WS{
-		clients: []*websocket.Conn{},
-	}
-}
