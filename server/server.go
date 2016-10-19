1a6160def0849167df20cf6bca08f1d6ff78cbaa
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-19 07:51:17 -0200

    ;

--- server/server.go
+++ server/server.go
@@ -1,13 +1,31 @@
 package server
 
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
+)
+
 func Start(opt *Options) error {
 	opt.Mount()
 
+	startServer(opt)
 	OpenBrowser(opt)
 
 	if err := StartWatcher(opt); err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func startServer(opt *Options) error {
+	e := echo.New()
+
+	e.Use(middleware.Static(""))
+	e.Use(middleware.Static("/" + opt.Only))
+
+	e.Run(standard.New(opt.Port))
 
 	return nil
 }
