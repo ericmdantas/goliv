f6c142c5896858a83db244cb577af26266c4d31a
Eric Mendes Dantas <ericdantas0@gmail.com>
2018-06-18 17:10:55 -0300

    .

--- main.go
+++ main.go
@@ -10,16 +10,16 @@ import (
 func main() {
 	cfg := server.NewConfig()
 
-	flag.StringVar(&cfg.Port, "port", "", "a string beginning with")
+	flag.StringVar(&cfg.Port, "port", "", "the port number begining with :")
 	flag.StringVar(&cfg.Host, "host", "", "the base of your HTTPURL")
-	flag.StringVar(&cfg.Root, "root", "", "the root of you app")
+	flag.StringVar(&cfg.Root, "root", "", "the root of your project")
 	flag.BoolVar(&cfg.HTTP2, "http2", false, "http2 server or not")
 	flag.BoolVar(&cfg.Quiet, "quiet", false, "no log")
-	flag.BoolVar(&cfg.OpenBrowser, "openBrowser", false, "open the browser")
+	flag.BoolVar(&cfg.OpenBrowser, "openBrowser", false, "opens the browser")
 	flag.StringVar(&cfg.OnlyCLI, "only", "", "watchable paths - separated by comma")
 	flag.StringVar(&cfg.IgnoreCLI, "ignore", "", "paths ignored - separated by comma")
 	flag.StringVar(&cfg.PathIndex, "pathIndex", "", "path to the index.html")
-	flag.BoolVar(&cfg.Proxy, "proxy", false, "if it's working as a reverse proxy or not")
+	flag.BoolVar(&cfg.Proxy, "proxy", false, "reverse proxy")
 	flag.StringVar(&cfg.ProxyWhen, "proxyWhen", "", "when to activate the proxy")
 	flag.StringVar(&cfg.ProxyTarget, "proxyTarget", "", "target server/endpoint")
 
